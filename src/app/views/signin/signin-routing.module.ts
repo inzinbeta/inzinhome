bc5fa3004364526afe44070c8861a9a6e87c832e
Shubham Dixit <shubhamdixit@Shubhams-MacBook-Air.local>
2020-02-16 20:16:50 +0530

    updated

---
+++ src/app/views/signin/signin-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+
+
+const routes: Routes = [
+
+  { 
+    path: '', 
+    component:SigninComponent, 
+    data: { title: 'NgX Table', breadcrumb: 'NgX Table' } 
+  },
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SigninRoutingModule { }
