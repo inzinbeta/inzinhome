82ec354bc965e69cd78407c7c11bbbeb2524c916
Shashank <shashank.jaitly.98@gmail.com>
2020-02-21 18:32:06 +0530

    to_add_dynamic_grid_for_products

--- src/app/views/index/index.module.ts
+++ src/app/views/index/index.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from "@angular/common";
 import { IndexRoutingModule } from "./index.routing";
 import { IndexComponent } from "./index/index.component";
 import { CarouselModule } from "ngx-owl-carousel-o";
+import { RatingModule } from "ng-starrating";
+import { AgGridModule } from "ag-grid-angular";
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, IndexRoutingModule, CarouselModule]
+  imports: [
+    CommonModule,
+    IndexRoutingModule,
+    CarouselModule,
+    RatingModule,
+    AgGridModule
+  ]
 })
 export class IndexModule {}
--- src/app/views/index/index/index.component.ts
+++ src/app/views/index/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { OwlOptions } from "ngx-owl-carousel-o";
+// import { OwlOptions } from "ngx-owl-carousel-o";
+import { StarRatingComponent } from "ng-starrating";
 
 @Component({
   selector: "app-index",
@@ -11,58 +12,93 @@ export class IndexComponent implements OnInit {
 
   ngOnInit() {}
 
-  slidesStore = [
-    {
-      id: 1,
-      src: "assets/img/home/cat1.jpg",
-      title: "cat1"
-    },
-    {
-      id: 2,
-      src: "assets/img/home/cat2.jpg",
-      title: "cat2"
-    },
-    {
-      id: 3,
-      src: "assets/img/home/cat3.jpg",
-      title: "cat3"
-    },
-    {
-      id: 4,
-      src: "assets/img/home/cat4.jpg",
-      title: "cat4"
-    },
-    {
-      id: 5,
-      src: "assets/img/home/cat5.jpg",
-      title: "cat5"
-    },
-    {
-      id: 6,
-      src: "assets/img/home/cat6.jpg",
-      title: "cat6"
-    },
-    {
-      id: 7,
-      src: "assets/img/home/cat7.jpg",
-      title: "cat7"
-    },
-    {
-      id: 8,
-      src: "assets/img/home/cat8.jpg",
-      title: "cat8"
-    }
-  ];
+  // slides = [
+  //   {
+  //     id: 1,
+  //     src: "assets/img/home/cat1.jpg",
+  //     title: "cat1",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 2,
+  //     src: "assets/img/home/cat2.jpg",
+  //     title: "cat2",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 3,
+  //     src: "assets/img/home/cat3.jpg",
+  //     title: "cat3",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 4,
+  //     src: "assets/img/home/cat4.jpg",
+  //     title: "cat4",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 5,
+  //     src: "assets/img/home/cat5.jpg",
+  //     title: "cat5",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 6,
+  //     src: "assets/img/home/cat6.jpg",
+  //     title: "cat6",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 7,
+  //     src: "assets/img/home/cat7.jpg",
+  //     title: "cat7",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   },
+  //   {
+  //     id: 8,
+  //     src: "assets/img/home/cat8.jpg",
+  //     title: "cat8",
+  //     desc:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  //     likes: 5
+  //   }
+  // ];
 
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    dots: false,
-    items: 4,
-    margin: 25,
-    center: true,
-    autoplayTimeout: 2500
-  };
+  // customOptions: OwlOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   mouseDrag: true,
+  //   touchDrag: true,
+  //   dots: false,
+  //   items: 4,
+  //   margin: 15,
+  //   center: true,
+  //   autoplayTimeout: 2500
+  // };
+
+  onRate($event: {
+    oldValue: number;
+    newValue: number;
+    starRating: StarRatingComponent;
+  }) {
+    alert(`Old Value:${$event.oldValue}, 
+      New Value: ${$event.newValue}, 
+      Checked Color: ${$event.starRating.checkedcolor}, 
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+  }
 }
