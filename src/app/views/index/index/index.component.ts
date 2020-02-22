ebf35e5f91b568c080b8e24a80a28ce89ff62f3e
Shashank <shashank.jaitly.98@gmail.com>
2020-02-22 10:43:42 +0530

    home_page_brands

--- src/app/views/index/index/index.component.ts
+++ src/app/views/index/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-// import { OwlOptions } from "ngx-owl-carousel-o";
+//import { OwlOptions } from "ngx-owl-carousel-o";
 import { StarRatingComponent } from "ng-starrating";
 
 @Component({
@@ -12,72 +12,72 @@ export class IndexComponent implements OnInit {
 
   ngOnInit() {}
 
-  // slides = [
-  //   {
-  //     id: 1,
-  //     src: "assets/img/home/cat1.jpg",
-  //     title: "cat1",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 2,
-  //     src: "assets/img/home/cat2.jpg",
-  //     title: "cat2",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 3,
-  //     src: "assets/img/home/cat3.jpg",
-  //     title: "cat3",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 4,
-  //     src: "assets/img/home/cat4.jpg",
-  //     title: "cat4",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 5,
-  //     src: "assets/img/home/cat5.jpg",
-  //     title: "cat5",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 6,
-  //     src: "assets/img/home/cat6.jpg",
-  //     title: "cat6",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 7,
-  //     src: "assets/img/home/cat7.jpg",
-  //     title: "cat7",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   },
-  //   {
-  //     id: 8,
-  //     src: "assets/img/home/cat8.jpg",
-  //     title: "cat8",
-  //     desc:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-  //     likes: 5
-  //   }
-  // ];
+  slides = [
+    {
+      id: 1,
+      src: "assets/img/home/cat1.jpg",
+      title: "cat1",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 2,
+      src: "assets/img/home/cat2.jpg",
+      title: "cat2",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 3,
+      src: "assets/img/home/cat3.jpg",
+      title: "cat3",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 4,
+      src: "assets/img/home/cat4.jpg",
+      title: "cat4",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 5,
+      src: "assets/img/home/cat5.jpg",
+      title: "cat5",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 6,
+      src: "assets/img/home/cat6.jpg",
+      title: "cat6",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 7,
+      src: "assets/img/home/cat7.jpg",
+      title: "cat7",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    },
+    {
+      id: 8,
+      src: "assets/img/home/cat8.jpg",
+      title: "cat8",
+      desc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+      likes: 5
+    }
+  ];
 
   // customOptions: OwlOptions = {
   //   loop: true,
